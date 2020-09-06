1e4667c1348960fa740285656a8897a29124614d
Xenon <67352482+thexenon@users.noreply.github.com>
2020-09-06 20:53:43 +0100

    Added Footer and Forgot Password

--- index.js
+++ index.js
@@ -51,3 +51,25 @@
       $("#container").html(data)
     });
   }
+
+  $("#btn-resetPassword").click(function()
+  {
+    var auth = firebase.auth();
+    var email = $("#fpass-email").val();
+    if (email != ""){
+      auth.sendPasswordResetEmail(email).then(function(){
+        window.alert("Email has been sent to your mail, Please check and verify");
+      })
+      .catch(function(error)
+      {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.log(errorCode);
+        console.log(errorMessage);
+        window.alert("Message : " + errorMessage);
+      });
+    } else {
+      window.alert("Please write your email first.");
+    }
+  });
